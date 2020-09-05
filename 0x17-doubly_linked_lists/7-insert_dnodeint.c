528e522eaf2a9cd05f527621c1b8fa6fb7648a90
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-05 19:23:09 +0000

    Fixing NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,9 +8,13 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux = *h, *new_node;
+	dlistint_t *aux, *new_node;
 	unsigned int counter = 0;
 
+	if (h == NULL)
+		return (NULL);
+
+	aux = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
