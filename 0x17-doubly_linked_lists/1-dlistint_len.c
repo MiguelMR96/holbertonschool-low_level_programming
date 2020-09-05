d5d9ddf53bc0b904d51d920e3022238bb7b8b918
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-05 17:17:14 +0000

    Fixing betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,12 @@ size_t recursion_count(const dlistint_t *h, size_t counter)
 {
 	if (h == NULL)
 		return (counter);
-	else
-	{
-		return (recursion_count(h->next, ++counter));
-	}
+
+	return (recursion_count(h->next, ++counter));
 }
+
 /**
- * dlistint_t - return number of elements in a linked list
+ * dlistint_len - return number of elements in a linked list
  * @h: head to doubly linked list
  * Return: number of elements
  */
