ff783d89c3a2324b55ca35d611b7961225335aa3
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-05 18:03:16 +0000

    Fixing lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -26,5 +27,6 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
 
 #endif /*lists_h*/
