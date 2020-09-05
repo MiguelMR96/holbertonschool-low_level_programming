f92bd0689b70129771e050e0660f6b1d70f8131b
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-05 19:50:33 +0000

    Fixing bugs

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	if (idx == 0 || *h == NULL)
+	if (idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
 		return (new_node);
