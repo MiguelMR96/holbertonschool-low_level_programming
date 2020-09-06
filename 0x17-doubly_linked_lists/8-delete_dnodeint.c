d0d4d03aa5fa758ac0152f1e6ad50a8fbd4c7b1b
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-06 18:32:39 +0000

    Delete dnodeint only one function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,24 +1,5 @@
 #include "lists.h"
 /**
- * del_dnodeint - delete node
- * @head: pointer to doubly linked list
- * @node: node to delete
- * Return: 1 succeeded, -1 if failed
- */
-int del_dnodeint(dlistint_t **head, dlistint_t *node)
-{
-	if (*head == node)
-		*head = node->next;
-	if (node->next)
-		(node->next)->prev = node->prev;
-	if (node->prev)
-		(node->prev)->next = node->next;
-
-	free(node);
-	return (1);
-}
-
-/**
  * delete_dnodeint_at_index - delete node at index
  * @head: pointer to doubly linke list's head
  * @index: node's index to delete
@@ -28,7 +9,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *aux;
 	unsigned int counter = 0;
-	int res = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -41,6 +21,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		counter++;
 	}
 
-	res = del_dnodeint(head, aux);
-	return (res);
+	if (*head == aux)
+		*head = aux->next;
+	if (aux->next)
+		(aux->next)->prev = aux->prev;
+	if (aux->prev)
+		(aux->prev)->next = aux->next;
+
+	free(aux);
+	return (1);
 }
