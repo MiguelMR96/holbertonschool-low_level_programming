6ecd4874d3a1401603fec5eaf701842021989a18
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-02 16:18:28 +0000

    0 task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *vec;
+
+	vec = malloc(sizeof(hash_table_t));
+	if (!vec)
+		return NULL;
+
+	vec->size = size;
+
+	vec->array = calloc(size, sizeof(hash_node_t *));
+	if (!(vec->array))
+		return NULL;
+
+	return (vec);
+}
