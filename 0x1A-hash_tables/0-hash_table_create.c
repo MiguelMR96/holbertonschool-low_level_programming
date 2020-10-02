779b77153154ccbda384a0acc4bbfd8d9842bcdf
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-02 16:36:06 +0000

    Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,18 +1,22 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creation of empty hash_table
+ * @size: hash table's size
+ * Return: hash table empty but created
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *vec;
 
 	vec = malloc(sizeof(hash_table_t));
 	if (!vec)
-		return NULL;
+		return (NULL);
 
 	vec->size = size;
 
 	vec->array = calloc(size, sizeof(hash_node_t *));
 	if (!(vec->array))
-		return NULL;
+		return (NULL);
 
 	return (vec);
 }
