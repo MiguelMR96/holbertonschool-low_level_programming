3a023393b47a580eb8ffc157dc6e1412654c2817
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-02 18:05:17 +0000

    2 task

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * key_index - finds the indez of a key
+ * @key: key
+ * @size: array's size
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0, hash = 0;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
+}
