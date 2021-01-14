675720dd77cf4d4d8454a61b1b35fd91f176426b
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-01-14 16:17:37 +0000

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - simple search algorithm
+ * for more info search linear algorithm in Google
+ * @array: sorted array
+ * @size: array´s size
+ * @value: value to find in array
+ * Return: int (value)
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+	int size_i = size;
+
+	if (!array)
+		return (-1);
+
+	while (i < size_i)
+	{
+		if (array[i] == value)
+			return (i);
+		printf("Value checked array[%d] = %d\n", i, array[i]);
+		i++;
+	}
+	return (-1);
+}
