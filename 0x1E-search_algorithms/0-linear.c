e00a05a2c9e9b251ec1c50b1c997728911270e3e
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-01-15 05:34:32 +0000

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!(array))
+	if (!array)
 		return (-1);
 
 	while (i < size)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		i++;
 	}
 	return (-1);
